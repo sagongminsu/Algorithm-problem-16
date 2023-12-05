c6c56520a2190f7d01b1799151fe94c4dfffe55b
사공민수 <149641823+sagongminsu@users.noreply.github.com>
2023-12-05 09:40:11 +0900

    16
    x만큼 간격이 있는 n개의 숫자

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public long[] solution(int x, int n)
+    {
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+                answer[i] = x;
+            else
+                answer[i] = x + answer[i - 1];
+        }
+
+        return answer;
+
+    }
+}
